3b52c9c3618d2503015bf23b9475f0fef79b68b1
alexshtf <none@none.com>
2011-04-24 08:58:21 +0000

    Update to AssemblyInfo and add a NuGet package

--- AutoDiff/AutoDiff/Properties/AssemblyInfo.cs
+++ AutoDiff/AutoDiff/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AutoDiff")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("High-performance and high-accuracy automatic function-differentiation library suitable for optimization and numeric computing.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("NA")]
+[assembly: AssemblyCompany("Alex Shtof")]
 [assembly: AssemblyProduct("AutoDiff")]
 [assembly: AssemblyCopyright("Copyright © NA 2010")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.3.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
