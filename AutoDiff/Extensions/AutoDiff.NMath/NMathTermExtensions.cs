1dc444607846e44fb99f15c694b1661d1c325d78
alexshtf <alex.shtf@gmail.com>
2012-10-08 20:09:57 +0200

    Added AutoDiff.NMath extension

---
+++ AutoDiff/Extensions/AutoDiff.NMath/NMathTermExtensions.cs
@@ -0,0 +1,14 @@
+﻿using CenterSpace.NMath.Core;
+
+namespace AutoDiff.NMath
+{
+    public static class NMathTermExtensions
+    {
+        public static DoubleFunctional AsDoubleFunctional(this Term term, params Variable[] variables)
+        {
+            var compiledTerm = term.Compile(variables);
+            var result = new CompiledTermDoubleFunctional(compiledTerm);
+            return result;
+        }
+    }
+}
