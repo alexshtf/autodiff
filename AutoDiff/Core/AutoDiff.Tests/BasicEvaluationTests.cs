2573e42050755d2b69bb334041782e268bbb4c7b
Alex Shtof <alex.shtf@gmail.com>
2018-01-24 10:45:57 +0200

    Improved consistency in test code
    Related to #3

--- AutoDiff/Core/AutoDiff.Tests/BasicEvaluationTests.cs
+++ AutoDiff/Core/AutoDiff.Tests/BasicEvaluationTests.cs
@@ -42,7 +42,7 @@ namespace AutoDiff.Tests
             var c = Constant(5);
             var v = new Variable();
             var sum = c + v;
-            var value = sum.Evaluate(Vec(v), Vec(7.0));
+            var value = sum.Evaluate(Vec(v), NumVec(7));
             Assert.Equal(12, value);
         }
 
@@ -62,7 +62,7 @@ namespace AutoDiff.Tests
             var c = Constant(12);
             var v = new Variable();
             var diff = c - v;
-            var value = diff.Evaluate(Vec(v), Vec(5.0));
+            var value = diff.Evaluate(Vec(v), NumVec(5));
             Assert.Equal(7, value);
         }
 
@@ -72,7 +72,7 @@ namespace AutoDiff.Tests
             var v1 = new Variable();
             var v2 = new Variable();
             var prod = v1 * v2;
-            var value = prod.Evaluate(Vec(v1, v2), Vec(3.0, -5));
+            var value = prod.Evaluate(Vec(v1, v2), NumVec(3, -5));
             Assert.Equal(-15, value);
         }
 
@@ -100,9 +100,8 @@ namespace AutoDiff.Tests
         {
             var v = new Variable();
             var sqDiff = Power(v - 5, 2);
-
-            var r1 = sqDiff.Evaluate(Vec(v), Vec(3.0));
-            var r2 = sqDiff.Evaluate(Vec(v), Vec(5.0));
+            var r1 = sqDiff.Evaluate(Vec(v), NumVec(3));
+            var r2 = sqDiff.Evaluate(Vec(v), NumVec(5));
 
             Assert.Equal(4, r1);
             Assert.Equal(0, r2);
@@ -117,9 +116,9 @@ namespace AutoDiff.Tests
                 3 * Power(v[1] - 4, 2),
                 2 * Power(v[2] + 3, 2));
 
-            var r1 = sqDiff.Evaluate(v, Vec(5.0, 4.0, -3.0));
-            var r2 = sqDiff.Evaluate(v, Vec(3.0, 4.0, -3.0));
-            var r3 = sqDiff.Evaluate(v, Vec(4.0, 4.0, 0.0));
+            var r1 = sqDiff.Evaluate(v, NumVec(5, 4, -3));
+            var r2 = sqDiff.Evaluate(v, NumVec(3, 4, -3));
+            var r3 = sqDiff.Evaluate(v, NumVec(4, 4, 0));
 
             Assert.Equal(0, r1);
             Assert.Equal(48, r2);
@@ -136,9 +135,9 @@ namespace AutoDiff.Tests
 
             var term = new UnaryFunc(eval, diff, v);
 
-            var y1 = term.Evaluate(Vec(v), Vec(1.0));
-            var y2 = term.Evaluate(Vec(v), Vec(2.0));
-            var y3 = term.Evaluate(Vec(v), Vec(3.0));
+            var y1 = term.Evaluate(Vec(v), NumVec(1));
+            var y2 = term.Evaluate(Vec(v), NumVec(2));
+            var y3 = term.Evaluate(Vec(v), NumVec(3));
 
             Assert.Equal(1.0, y1);
             Assert.Equal(4.0, y2);
@@ -155,9 +154,9 @@ namespace AutoDiff.Tests
             // f(x, y) = x^2 + 2 * y^2
             var term = square(v[0]) +  2 * square(v[1]);
 
-            var y1 = term.Evaluate(v, Vec(1.0, 0.0));  // 1 + 0 = 1
-            var y2 = term.Evaluate(v, Vec(0.0, 1.0));  // 0 + 2 = 2
-            var y3 = term.Evaluate(v, Vec(2.0, 1.0));  // 4 + 2 = 6
+            var y1 = term.Evaluate(v, NumVec(1, 0));  // 1 + 0 = 1
+            var y2 = term.Evaluate(v, NumVec(0, 1));  // 0 + 2 = 2
+            var y3 = term.Evaluate(v, NumVec(2, 1));  // 4 + 2 = 6
 
             Assert.Equal(1, y1);
             Assert.Equal(2, y2);
@@ -174,9 +173,9 @@ namespace AutoDiff.Tests
 
             var term = func(v[0], v[1]);
 
-            var y1 = term.Evaluate(v, Vec(1.0, 0.0)); // 1 - 0 = 1
-            var y2 = term.Evaluate(v, Vec(0.0, 1.0)); // 0 - 0 = 0
-            var y3 = term.Evaluate(v, Vec(1.0, 2.0)); // 1 - 2 = -1
+            var y1 = term.Evaluate(v, NumVec(1, 0)); // 1 - 0 = 1
+            var y2 = term.Evaluate(v, NumVec(0, 1)); // 0 - 0 = 0
+            var y3 = term.Evaluate(v, NumVec(1, 2)); // 1 - 2 = -1
 
             Assert.Equal(1.0, y1);
             Assert.Equal(0.0, y2);
@@ -194,9 +193,9 @@ namespace AutoDiff.Tests
             // f(x, y) = x² - xy - y² + xy = x² - y²
             var term = func(v[0], v[1]) - func(v[1], v[0]);
 
-            var y1 = term.Evaluate(v, Vec(1.0, 0.0)); // 1 - 0 = 1
-            var y2 = term.Evaluate(v, Vec(0.0, 1.0)); // 0 - 1 = -1
-            var y3 = term.Evaluate(v, Vec(2.0, 1.0)); // 4 - 1 = 3
+            var y1 = term.Evaluate(v, NumVec(1, 0)); // 1 - 0 = 1
+            var y2 = term.Evaluate(v, NumVec(0, 1)); // 0 - 1 = -1
+            var y3 = term.Evaluate(v, NumVec(2, 1)); // 4 - 1 = 3
 
             Assert.Equal(1.0, y1);
             Assert.Equal(-1.0, y2);
